d7ed26eb661f2751d69c03fcfceabe78ad0628d6
Randy Parker <randallbparker@gmail.com>
2020-10-01 08:52:20 -0500

    fixed bug with ToLower char count

--- 3-8-Studio-Counting-Characters/Program.cs
+++ 3-8-Studio-Counting-Characters/Program.cs
@@ -12,11 +12,14 @@ namespace _3_8_Studio_Counting_Characters
             Console.WriteLine(input);
             Console.WriteLine("_______________________");
 
-            IEnumerable<char> uniqueChars = input.ToLower().Distinct(); // Distinct removes duplicates, ToLower makes this case insensitive
+            string inputStr = input.ToLower(); // ToLower makes our search case insensitive
+
+            IEnumerable<char> uniqueChars = inputStr.Distinct(); // Distinct removes duplicates
+
             Dictionary<char, int> charCount = new Dictionary<char, int>();
 
             foreach (char character in uniqueChars) {
-                int count = input.Count(a => a == character && Regex.IsMatch(character.ToString(), @"^[a-zA-Z]+$")); // our friend Regex prevents non-alphabetic characters from incrementing
+                int count = inputStr.Count(a => a == character && Regex.IsMatch(character.ToString(), @"^[a-zA-Z]+$")); // our friend Regex prevents non-alphabetic characters from incrementing
 
                 if ( count > 0) { // Thanks to Regex, non alphabetic characters are represented by 0 count .. could have done this a few different ways but this works
                     charCount.Add(character, count);  // since alphabetic characters have a positive count we Add them to our dictionary
@@ -35,6 +38,7 @@ namespace _3_8_Studio_Counting_Characters
         {
             countChars("Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc accumsan sem ut ligula scelerisque sollicitudin. Ut at sagittis augue. Praesent quis rhoncus justo. Aliquam erat volutpat. Donec sit amet suscipit metus, non lobortis massa. Vestibulum augue ex, dapibus ac suscipit vel, volutpat eget massa. Donec nec velit non ligula efficitur luctus.");
             countChars("abcdefghijklmnopqrstuvwxyz.!01234567890 ?");
+            countChars("aBbCcCdDdDeEeEe");
         }
     }
 }
